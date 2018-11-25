d41d6b30b9c232eb27432509bb18494471c7c4a3
Olga <olga.myazina@outlook.com>
2018-11-25 21:10:35 +0300

    feat: firstChildAccordion

--- my-app/src/components/TileBox/TileBox.tsx
+++ my-app/src/components/TileBox/TileBox.tsx
@@ -1,4 +1,5 @@
-import { BorderBox, Box } from '@primer/components';
+import Octicon, { ChevronDown, ChevronUp } from '@githubprimer/octicons-react';
+import { BorderBox, Box, Button, Flex } from '@primer/components';
 import 'primer-box/index.scss';
 import * as React from 'react';
 import './TileBox.css';
@@ -6,27 +7,55 @@ import './TileBox.css';
 interface ITileBoxProps {
   name: string;
 }
+/*
+interface IButtonProps {
+  open: string;
+  toggle: string;
+}*/
 
-const TileBox: React.SFC<ITileBoxProps> = props => {
-  return (
-    <React.Fragment>
-      <Box className="Box">
+interface ITileBoxState {
+  isOpen: boolean;
+}
+
+class TileBox extends React.Component<ITileBoxProps, ITileBoxState> {
+  constructor(props: ITileBoxProps) {
+    super(props);
+    this.state = { isOpen: true };
+    this.toggle = this.toggle.bind(this);
+  }
+  public isIconOpen(open: any) {
+    return open ? ChevronUp : ChevronDown;
+  }
+  public toggle() {
+    if (this.state.isOpen) {
+      this.setState({ isOpen: false });
+    } else {
+      this.setState({ isOpen: true });
+    }
+  }
+
+  public render() {
+    return (
+      <React.Fragment>
         <BorderBox ml="3" mr="3">
-          <div className="Box-header">
-            <h3 className="Box-title">Box title</h3>
-          </div>
-          <p>Text in p inner Box</p>
-          <div className="Box-title">textTitle</div>
+          <Box is="div" width="100%">
+            <div className="Box-header">
+              <Flex justifyContent="space-between">
+                <h3 className="Box-title">Box title</h3>
+                <Button is="summary" onClick={this.toggle}>
+                  <Octicon icon={this.state.isOpen ? ChevronUp : ChevronDown} />
+                </Button>
+              </Flex>
+            </div>
+          </Box>
+
+          <Box is="div" width="100%">
+            <div className={`Box-body${this.state.isOpen ? '' : '_isHidden'}`}>textTitle</div>
+          </Box>
         </BorderBox>
-        <BorderBox ml="3" mr="3">
-          <div className="Box-row--gray">
-            <h3 className="Box-title">Box title</h3>
-          </div>
-          <p>Text in p inner Box</p>
-          <div className="Box-title">textTitle</div>
-        </BorderBox>
-      </Box>
-    </React.Fragment>
-  );
-};
+      </React.Fragment>
+    );
+  }
+}
+
 export default TileBox;
